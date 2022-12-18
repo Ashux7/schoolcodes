d39b82a866b2a1ac2a43dbd5e325bfa57bedda09
Ashutosh <prajapatiashu777@gmail.com>
2022-12-18 22:47:39 +0530

    changed name

---
+++ heh.py
@@ -0,0 +1,28 @@
+# l=[23,4324]
+# lis=['sdf','sdf','ght']
+# print(lis+l)
+# lis.append(l)
+# print(lis)
+# print(len(lis))
+# lis.extend(l)
+# print(lis)
+# lis.insert(0,l)
+# print(lis)
+
+# dic
+
+# dic={}
+# for i in range(4):
+#     rollno = int(input("Enter roll no"))
+#     marks = int(input("Enter marks"))
+#     dic[rollno]=marks
+# print (dic)
+
+# d={2:'two',3:'teen'}
+# for i in d:
+#     print(i,d[i])
+# print("M chutiya hu, maine kaha ki \ndjdj ")
+d={3:'int',2.0:'float',True:'Bool'}
+# for i in d:
+#     print(i,d[i])
+print(d.keys(),d.values())
